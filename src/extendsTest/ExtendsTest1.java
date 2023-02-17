7ca3fc6fe9388690c09ac4da4eef617ca6d9301b
xiaohe <3128324742@qq.com>
2023-02-17 21:27:50 +0800

    第二次提交

--- src/extendsTest/ExtendsTest1.java
+++ src/extendsTest/ExtendsTest1.java
@@ -1,2 +1,30 @@
-package extendsTest;public class ExtendsTest1 {
+package extendsTest;
+public class ExtendsTest1 {
+    public static void main(String[] args) {
+        LOL l = new LOL();
+        l.update();
+        l.start();
+    }
+}
+
+class Game {
+    public void start() {
+        System.out.println("游戏启动了");
+    }
+}
+
+class PCGame extends Game {
+    public void update() {
+        System.out.println("PCGame更新了");
+    }
+}
+
+class MobileGame extends Game {
+    public void update() {
+        System.out.println("MobileGame更新了");
+    }
+}
+
+class LOL extends PCGame {
+
 }
